707e1b03d74b4af588a2c2b7694b5537972119ea
Barrack <barrackdome45@gmail.com>
2025-09-13 13:59:55 +0300

    Added finance_calculator.py file

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,8 @@
+income = float(input("Enter your monthly income: "))
+expenses = float(input("Enter your total monthy expenses: "))
+
+savings_monthly = income - expenses
+savings_yearly = savings_monthly * 12
+
+interest_rate = 0.05
+savings_with_interest = savings_yearly * (1 + interest_rate)
