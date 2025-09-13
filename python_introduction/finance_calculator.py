a02deedd81d667987af6f72b5f9d196b80619853
Barrack Dome <barrackdome45@gmail.com>
2025-09-13 14:42:33 +0300

    Update finance_calculator.py

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -6,3 +6,6 @@ savings_yearly = savings_monthly * 12
 
 interest_rate = 0.05
 savings_with_interest = savings_yearly * (1 + interest_rate)
+
+print("Your monthly savings are $%.2f." % savings_monthly)
+print("Project savings after one year , with interest, is: $%.2f." % savings_with_interest)
